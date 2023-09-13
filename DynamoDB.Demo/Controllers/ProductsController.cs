c9adebcf5542ee26eb94e49d90f9e5bed0305e3d
Pothirajan Chithiravelu <UX011789@TCS-SG1D155>
2023-09-13 19:11:21 +0530

    Add project files.

---
+++ DynamoDB.Demo/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamoDB.Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IDynamoDBContext _context;
+
+        public ProductsController(IDynamoDBContext context)
+        {
+            _context = context;
+            
+        }
+        [HttpGet("{id}/{barcode}")]
+        public async Task<IActionResult> Get(string id, string barcode)
+        {
+            var product = await _context.LoadAsync<Product>(id, barcode);
+            if (product == null) { return NotFound(); }
+            return Ok(product);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var product = await _context.ScanAsync<Product>(default).GetRemainingAsync();
+            return Ok(product);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Product request)
+        {
+            var product = await _context.LoadAsync<Product>(request.Id, request.Barcode);
+            if (product != null) { return BadRequest($"Product with Id {request.Id} and Barcode {request.Barcode} already exists"); };
+            await _context.SaveAsync(request);
+            return NoContent();
+        }
+    }
+}
